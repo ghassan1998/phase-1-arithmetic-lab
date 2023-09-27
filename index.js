35ac54f0a9958382dc49e7cc4498a906a22ed1b6
Ghassan Ajouri <nghassan1998@gmail.com>
2023-09-27 19:14:21 +0300

    Done

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+
+function generateRandomInteger() {
+  return Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+}
+
+let random = generateRandomInteger();
+
+
+let num3 = 19;
+let num4 = 5;
+let mod = num3 % num4;
+
+
+let numbers = [10, 20, 15, 5];
+let max = Math.max(...numbers);
+
+console.log("multiply:", multiply); 
+console.log("random:", random);     
+console.log("mod:", mod);           
+console.log("max:", max);           
